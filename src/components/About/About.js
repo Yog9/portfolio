e19fbe7f62e927b363268564033a4a6fb7b97383
Yog9 <veryogita@gmail.com>
2019-07-02 23:10:36 +0530

    Projects added

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import './About.css';
-import { Link } from "react-router-dom";
 export default function About() {
     return (
         <div className="mainDiv">
@@ -10,7 +9,7 @@ export default function About() {
                     <h3 className="pageHeader">Background</h3>
                     <p>I am a front-end developer from Pune,Maharashtra.I'm doing web development and I'm on my way to become a Full Stack Engineer.I have also worked at BAI Infosystems Private Limited, Pune as Web Designer for 4 months.</p>
                     <h3>Knowledgle In</h3>
-                    <p>I'm knowledgeble in JavaScript,React, Redux,Nodejs and Sass.I invite you to take a look at my <Link to="/https://github.com/Yog9/" target="_blank">Github </Link> to see examples of the many self-initiated projects that I have completed.</p>
+                    <p>I'm knowledgeble in JavaScript,React, Redux,Nodejs and Sass.I invite you to take a look at my <a href="/https://github.com/Yog9/" target="_blank">Github </a> to see examples of the many self-initiated projects that I have completed.</p>
                 </section>
 
             </article>
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,40 +1,62 @@
 import React from 'react'
 import './Projects.css';
-import { Link } from "react-router-dom";
 export default function Projects() {
     return (
         <div className="mainDiv">
             <div className="project">
                 <div className="project-list">
                     <div className="project-item">
-                        <h3>Movie Flex (On-going)</h3>
-                        <p><strong>Description:</strong> Browse your favorite Movies, TV shows. Search for specific movies and TV shows.Browse the latest and greatest films and to find information about them.</p>
+                        <h3>Movie Flex</h3>
+                        <p><strong>Description:</strong>Movie Flex is a movie application created with React/Redux in which users can browse Movies and TV shows from TMDB API to find more information about them. Also provided a search functionality for searching movies/TV shows.</p>
                         <p><strong> Technologies Used:</strong> JavaScript, ReactJS, Redux, CSS3.</p>
                         <div className="buttons">
-                            <Link to="https://github.com/Yog9/Movie-Flex" target="_blank">Souce Code</Link>
-                            <Link to="" target="_blank">Live Demo</Link>
+                            <a href="https://github.com/Yog9/Movie-Flex" target="_blank">Souce Code</a>
+                            <a href="https://yog9.github.io/Movie-Flex" target="_blank">Live Demo</a>
                         </div>
                     </div>
 
                     <div className="project-item">
                         <h3>Snap Scout</h3>
-                        <p><strong>Description:</strong> Snap Scout is React Gallery created using React. And using React Router the routes were setup for four default pages and a search page. Also the images were displayed using Flickr API using axios to fetch the data. </p>
+                        <p><strong>Description:</strong>Snap Scout is a gallery created using React and React Router. The Routes were setup for four default pages and a search page. Also the images were displayed using the Flickr API and axios to fetch data.</p>
                         <p><strong> Technologies Used:</strong> JavaScript, ReactJS, CSS3</p>
                         <div className="buttons">
-                            <Link to="https://github.com/Yog9/SnapScout" target="_blank">Souce Code</Link>
-                            <Link to="https://yog9.github.io/SnapScout/" target="_blank">Live Demo</Link>
+                            <a href="https://github.com/Yog9/SnapScout" target="_blank">Souce Code</a>
+                            <a href="https://yog9.github.io/SnapScout/" target="_blank">Live Demo</a>
                         </div>
                     </div>
 
                     <div className="project-item">
                         <h3>Express Portfolio</h3>
-                        <p><strong>Description:</strong>Express Portfolio showcases the projects that I have built. It stores all the data about the project in a JSON file. It uses Pug to generate a template that utilizes JSON to generate a markup that is displayed in the browser.</p>
-                        <p><strong>Technologies Used:</strong>Nodejs,Express,Pug</p>
+                        <p><strong>Description:</strong>Express Portfolio showcases the projects that I have built. It stores all the data about the projects in a JSON file. It uses Pug to generate a template that utilizes JSON to generate a markup that is displayed in the browser.</p>
+                        <p><strong>Technologies Used:</strong>Nodejs, Express, Pug, Foundation</p>
                         <div className="buttons">
-                            <Link to="https://github.com/Yog9/Express-Portfolio/" target="_blank">Souce Code</Link>
-                            <Link to="https://express-portfolio1.herokuapp.com/" target="_blank">Live Demo</Link>
+                            <a href="https://github.com/Yog9/Express-Portfolio/" target="_blank">Souce Code</a>
+                            <a href="https://express-portfolio1.herokuapp.com/" target="_blank">Live Demo</a>
                         </div>
                     </div>
+
+
+                    <div className="project-item">
+                        <h3>Bill Generator</h3>
+                        <p><strong>Description:</strong>Bill Generator is a full-stack app made to generate bill for food-items that the user orders. It is built from scratch using React, React Router, Node js and a NoSQL database mongo DB.</p>
+                        <p><strong>Technologies Used:</strong>React, React Router, Nodejs, Mongo DB</p>
+                        <div className="buttons">
+                            <a href="https://github.com/Yog9/Bill-Generator" target="_blank">Souce Code</a>
+                            <a href="https://express-portfolio1.herokuapp.com/" target="_blank">Live Demo</a>
+                        </div>
+                    </div>
+
+
+                    <div className="project-item">
+                        <h3>Shopping Cart</h3>
+                        <p><strong>Description:</strong>Shopping Cart is an ecommerce website made with react. User can shop for shirts and also add and remove items from  the cart.</p>
+                        <p><strong>Technologies Used:</strong>React, React Router</p>
+                        <div className="buttons">
+                            <a href="https://github.com/Yog9/shopping-cart" target="_blank">Souce Code</a>
+                            <a href="https://yog9.github.io/shopping-cart/" target="_blank">Live Demo</a>
+                        </div>
+                    </div>
+
                 </div>
             </div>
         </div>
