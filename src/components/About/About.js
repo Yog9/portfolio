595c9fe3765569dca68d6dbacd1038e0bf100808
Yog9 <veryogita@gmail.com>
2019-07-03 00:22:25 +0530

    Projects css udated

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,7 +7,7 @@ export default function About() {
                 <h2 className="aboutheading">
                     About Me</h2><section className="">
                     <h3 className="pageHeader">Background</h3>
-                    <p>I am a front-end developer from Pune,Maharashtra.I'm doing web development and I'm on my way to become a Full Stack Engineer.I have also worked at BAI Infosystems Private Limited, Pune as Web Designer for 4 months.</p>
+                    <p>I am a front-end developer from Pune, Maharashtra. I'm doing web development and I'm on my way to become a Full Stack Engineer. I have also worked at BAI Infosystems Private Limited, Pune as Web Designer for 4 months.</p>
                     <h3>Knowledgle In</h3>
                     <p>I'm knowledgeble in JavaScript,React, Redux,Nodejs and Sass.I invite you to take a look at my <a href="/https://github.com/Yog9/" target="_blank">Github </a> to see examples of the many self-initiated projects that I have completed.</p>
                 </section>
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -10,8 +10,8 @@ export default function Projects() {
                         <p><strong>Description:</strong>Movie Flex is a movie application created with React/Redux in which users can browse Movies and TV shows from TMDB API to find more information about them. Also provided a search functionality for searching movies/TV shows.</p>
                         <p><strong> Technologies Used:</strong> JavaScript, ReactJS, Redux, CSS3.</p>
                         <div className="buttons">
-                            <a href="https://github.com/Yog9/Movie-Flex" target="_blank">Souce Code</a>
-                            <a href="https://yog9.github.io/Movie-Flex" target="_blank">Live Demo</a>
+                            <a href="https://github.com/Yog9/Movie-Flex" rel="noopener noreferrer" target="_blank">Souce Code</a>
+                            <a href="https://yog9.github.io/Movie-Flex" rel="noopener noreferrer" target="_blank">Live Demo</a>
                         </div>
                     </div>
 
@@ -20,8 +20,8 @@ export default function Projects() {
                         <p><strong>Description:</strong>Snap Scout is a gallery created using React and React Router. The Routes were setup for four default pages and a search page. Also the images were displayed using the Flickr API and axios to fetch data.</p>
                         <p><strong> Technologies Used:</strong> JavaScript, ReactJS, CSS3</p>
                         <div className="buttons">
-                            <a href="https://github.com/Yog9/SnapScout" target="_blank">Souce Code</a>
-                            <a href="https://yog9.github.io/SnapScout/" target="_blank">Live Demo</a>
+                            <a href="https://github.com/Yog9/SnapScout" rel="noopener noreferrer" target="_blank">Souce Code</a>
+                            <a href="https://yog9.github.io/SnapScout/" rel="noopener noreferrer" target="_blank">Live Demo</a>
                         </div>
                     </div>
 
@@ -30,8 +30,8 @@ export default function Projects() {
                         <p><strong>Description:</strong>Express Portfolio showcases the projects that I have built. It stores all the data about the projects in a JSON file. It uses Pug to generate a template that utilizes JSON to generate a markup that is displayed in the browser.</p>
                         <p><strong>Technologies Used:</strong>Nodejs, Express, Pug, Foundation</p>
                         <div className="buttons">
-                            <a href="https://github.com/Yog9/Express-Portfolio/" target="_blank">Souce Code</a>
-                            <a href="https://express-portfolio1.herokuapp.com/" target="_blank">Live Demo</a>
+                            <a href="https://github.com/Yog9/Express-Portfolio/" rel="noopener noreferrer" target="_blank">Souce Code</a>
+                            <a href="https://express-portfolio1.herokuapp.com/"  rel="noopener noreferrer" target="_blank">Live Demo</a>
                         </div>
                     </div>
 
@@ -41,8 +41,8 @@ export default function Projects() {
                         <p><strong>Description:</strong>Bill Generator is a full-stack app made to generate bill for food-items that the user orders. It is built from scratch using React, React Router, Node js and a NoSQL database mongo DB.</p>
                         <p><strong>Technologies Used:</strong>React, React Router, Nodejs, Mongo DB</p>
                         <div className="buttons">
-                            <a href="https://github.com/Yog9/Bill-Generator" target="_blank">Souce Code</a>
-                            <a href="https://express-portfolio1.herokuapp.com/" target="_blank">Live Demo</a>
+                            <a href="https://github.com/Yog9/Bill-Generator" rel="noopener noreferrer" target="_blank">Souce Code</a>
+                            <a href="https://express-portfolio1.herokuapp.com/" rel="noopener noreferrer" target="_blank">Live Demo</a>
                         </div>
                     </div>
 
@@ -52,8 +52,8 @@ export default function Projects() {
                         <p><strong>Description:</strong>Shopping Cart is an ecommerce website made with react. User can shop for shirts and also add and remove items from  the cart.</p>
                         <p><strong>Technologies Used:</strong>React, React Router</p>
                         <div className="buttons">
-                            <a href="https://github.com/Yog9/shopping-cart" target="_blank">Souce Code</a>
-                            <a href="https://yog9.github.io/shopping-cart/" target="_blank">Live Demo</a>
+                            <a href="https://github.com/Yog9/shopping-cart" rel="noopener noreferrer" target="_blank">Souce Code</a>
+                            <a href="https://yog9.github.io/shopping-cart/" rel="noopener noreferrer" target="_blank">Live Demo</a>
                         </div>
                     </div>
 
