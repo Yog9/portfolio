75b2d2c5cebc6aecbac392b1a1cf4984f645f0c2
Yog9 <veryogita@gmail.com>
2019-07-03 18:52:47 +0530

    GA added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import ReactGA from 'react-ga';
 import Navbar from './components/Navbar/Navbar';
 import { HashRouter, Route, Switch } from "react-router-dom";
 import './App.css';
@@ -6,7 +7,12 @@ import Home from './components/Home/Home';
 import Contact from './components/Contact/Contact';
 import About from './components/About/About';
 import Projects from './components/Projects/Projects';
+ReactGA.initialize('UA-143242101-1');
+      ReactGA.pageview('/');
+      ReactGA.pageview("/projects");
+
 class App extends Component {
+ 
   render() {
     return (
       <HashRouter basename="/portfolio">
