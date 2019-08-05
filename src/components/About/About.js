bead44ab27b838fdef55e91c7170f58a5264637c
Yog9 <veryogita@gmail.com>
2019-08-05 09:14:33 +0530

    desc changed

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,29 +1,42 @@
-import React from 'react'
-import './About.css';
+import React from "react";
+import "./About.css";
 export default function About() {
-    return (
-        <div className="mainDiv">
-            <article className="about">
-                <h2 className="aboutheading">
-                    About Me</h2><section className="">
-                    <h3 className="pageHeader">Background</h3>
-                    <p>I am a front-end developer. I am currently working as a Software Developer at <a href="https://www.pragyaam.in/" rel="noopener noreferrer" target="_blank">Pragyaam</a>, Bengaluru. I am proficient in React, Redux and JavaScript.</p>
-                    <h3>Skills</h3>
-                    <ul>
-                        <li>JavaScript</li>
-                        <li>React</li>
-                        <li>Redux</li>
-                        <li>Node js(Express)</li>
-                        <li>Mongo DB</li>
-                        <li>CSS3</li>
-                        <li>Sass</li>
-                        <li>BootStrap</li>
-                        <li>Git</li>
-                        </ul>
-                    <p>I invite you to take a look at my <a href="https://github.com/Yog9/" rel="noopener noreferrer" target="_blank">Github </a> to see examples of the many self-initiated projects that I have completed.</p>
-                </section>
-
-            </article>
-        </div>
-    )
+  return (
+    <div className="mainDiv">
+      <article className="about">
+        <h2 className="aboutheading">About Me</h2>
+        <section className="">
+          <h3 className="pageHeader">Background</h3>
+          <p>
+            I am a front-end developer. I am currently looking for full-time
+            internships/jobs.
+          </p>
+          <h3>Skills</h3>
+          <ul>
+            <li>JavaScript</li>
+            <li>React</li>
+            <li>Redux</li>
+            <li>Node js(Express)</li>
+            <li>Mongo DB</li>
+            <li>CSS3</li>
+            <li>Sass</li>
+            <li>BootStrap</li>
+            <li>Git</li>
+          </ul>
+          <p>
+            I invite you to take a look at my{" "}
+            <a
+              href="https://github.com/Yog9/"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Github{" "}
+            </a>{" "}
+            to see examples of the many self-initiated projects that I have
+            completed.
+          </p>
+        </section>
+      </article>
+    </div>
+  );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,22 +1,31 @@
-import React, { Component } from 'react';
-import './Navbar.css';
+import React, { Component } from "react";
+import "./Navbar.css";
 import { Link } from "react-router-dom";
-import Resume from "../../assets/Resume.pdf"
+import Resume from "../../assets/resume.pdf";
 export default class Navbar extends Component {
-
-    render() {
-        return (
-            <div className="nav">
-                <Link to="/"><div className="brandname">YV</div></Link>
-                <div className="nav-menu">
-                    <ul>
-                        <Link to="/about"><li>About</li></Link>
-                        <Link to="/projects"><li>Projects</li></Link>
-                        <Link to="/contact"><li>Contact</li></Link>
-                        <a href = {Resume} rel="noopener noreferrer" target="_blank"><li>Resume</li></a>
-                    </ul>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="nav">
+        <Link to="/">
+          <div className="brandname">YV</div>
+        </Link>
+        <div className="nav-menu">
+          <ul>
+            <Link to="/about">
+              <li>About</li>
+            </Link>
+            <Link to="/projects">
+              <li>Projects</li>
+            </Link>
+            <Link to="/contact">
+              <li>Contact</li>
+            </Link>
+            <a href={Resume} rel="noopener noreferrer" target="_blank">
+              <li>Resume</li>
+            </a>
+          </ul>
+        </div>
+      </div>
+    );
+  }
 }
