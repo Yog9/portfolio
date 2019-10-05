122bb95ef9f923a1e18243eb29a89102c21ea51d
Yog9 <veryogita@gmail.com>
2019-10-05 22:21:17 +0530

    details changed

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,26 +4,21 @@ export default function About() {
   return (
     <div className="mainDiv">
       <article className="about">
-        <h2 className="aboutheading">About Me</h2>
+        <h2 className="aboutheading"> About Me </h2>{" "}
         <section className="">
-          <h3 className="pageHeader">Background</h3>
+          <h3 className="pageHeader"> Background </h3>{" "}
           <p>
-            I am a front-end developer. I am currently looking for full-time
-            internships/jobs.
-          </p>
-          <h3>Skills</h3>
+            I am a front-end developer. I am currently working as SDE at Ignite
+            Solutions, Pune
+          </p>{" "}
+          <h3> Skills </h3>{" "}
           <ul>
-            <li>JavaScript</li>
-            <li>React</li>
-            <li>Redux</li>
-            <li>Node js(Express)</li>
-            <li>Mongo DB</li>
-            <li>CSS3</li>
-            <li>Sass</li>
-            <li>BootStrap</li>
-            <li>Git</li>
-          </ul>
-          <p>
+            <li> JavaScript </li> <li> React </li> <li> Redux </li>{" "}
+            <li> Node js(Express) </li> <li> Angular / Angular.js </li>{" "}
+            <li> Mongo DB </li> <li> CSS3 </li> <li> Sass </li>{" "}
+            <li> BootStrap </li> <li> Git </li>{" "}
+          </ul>{" "}
+          {/* <p>
             I invite you to take a look at my{" "}
             <a
               href="https://github.com/Yog9/"
@@ -32,11 +27,11 @@ export default function About() {
             >
               Github{" "}
             </a>{" "}
-            to see examples of the many self-initiated projects that I have
-            completed.
-          </p>
-        </section>
-      </article>
+            to see examples of the many self - initiated projects that I have
+            completed.{" "}
+          </p>{" "} */}
+        </section>{" "}
+      </article>{" "}
     </div>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,30 +1,30 @@
 import React, { Component } from "react";
 import "./Navbar.css";
 import { Link } from "react-router-dom";
-import Resume from "../../assets/resume.pdf";
+import Resume from "../../assets/Resume.pdf";
 export default class Navbar extends Component {
   render() {
     return (
       <div className="nav">
         <Link to="/">
-          <div className="brandname">YV</div>
-        </Link>
+          <div className="brandname"> YV </div>{" "}
+        </Link>{" "}
         <div className="nav-menu">
           <ul>
             <Link to="/about">
-              <li>About</li>
-            </Link>
+              <li> About </li>{" "}
+            </Link>{" "}
             <Link to="/projects">
-              <li>Projects</li>
-            </Link>
+              <li> Projects </li>{" "}
+            </Link>{" "}
             <Link to="/contact">
-              <li>Contact</li>
-            </Link>
+              <li> Contact </li>{" "}
+            </Link>{" "}
             <a href={Resume} rel="noopener noreferrer" target="_blank">
-              <li>Resume</li>
-            </a>
-          </ul>
-        </div>
+              <li> Resume </li>{" "}
+            </a>{" "}
+          </ul>{" "}
+        </div>{" "}
       </div>
     );
   }
