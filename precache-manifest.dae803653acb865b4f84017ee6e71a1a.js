c95591bac4270ebd350bd220b9db4d5e6c6626e7
Yog9 <veryogita@gmail.com>
2019-10-05 22:26:14 +0530

    Updates

---
+++ precache-manifest.dae803653acb865b4f84017ee6e71a1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78c0bdd9f934e0db93e466a8f725b5af",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "13452d2813daa7c8a9b3",
+    "url": "/portfolio/static/css/main.c9d97af7.chunk.css"
+  },
+  {
+    "revision": "b0cd5f9640548cda35a4",
+    "url": "/portfolio/static/js/2.c32267c0.chunk.js"
+  },
+  {
+    "revision": "13452d2813daa7c8a9b3",
+    "url": "/portfolio/static/js/main.7d575efa.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "77301b3a1c45c17e8809e29643a5eca6",
+    "url": "/portfolio/static/media/Resume.77301b3a.pdf"
+  }
+]);
