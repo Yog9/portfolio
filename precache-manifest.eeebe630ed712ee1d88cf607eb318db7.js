31652607fdb013ac126c4ad0ded480b618a9858f
Yog9 <veryogita@gmail.com>
2020-02-13 11:59:01 +0530

    Updates

---
+++ precache-manifest.eeebe630ed712ee1d88cf607eb318db7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2abdd66ce7ec6e024348bb7397c29a9",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "d2c3a2f02f21a73e41ca",
+    "url": "/portfolio/static/css/main.f0587de1.chunk.css"
+  },
+  {
+    "revision": "adca85318f82674ed517",
+    "url": "/portfolio/static/js/2.a906d523.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/portfolio/static/js/2.a906d523.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2c3a2f02f21a73e41ca",
+    "url": "/portfolio/static/js/main.e2942e62.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "65707f333a7d8627867f46a17545aeb2",
+    "url": "/portfolio/static/media/codewars.65707f33.svg"
+  },
+  {
+    "revision": "55c0d25ee7f1f0c16019463e9a1a6c36",
+    "url": "/portfolio/static/media/resume.55c0d25e.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.a3f49b0d142cb01bccb645c73f699273.js"
+  "/portfolio/precache-manifest.eeebe630ed712ee1d88cf607eb318db7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
