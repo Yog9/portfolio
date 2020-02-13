d0fc0705dffbbe04f457553c1c195bff3d331f3c
Yog9 <veryogita@gmail.com>
2020-02-13 11:45:27 +0530

    App component updated

--- src/App.js
+++ src/App.js
@@ -1,33 +1,26 @@
-import React, { Component } from 'react'
-import ReactGA from 'react-ga';
-import Navbar from './components/Navbar/Navbar';
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
 import { HashRouter, Route, Switch } from "react-router-dom";
-import './App.css';
-import Home from './components/Home/Home';
-import Contact from './components/Contact/Contact';
-import About from './components/About/About';
-import Projects from './components/Projects/Projects';
-ReactGA.initialize('UA-143242101-1');
-      ReactGA.pageview('/portfolio/#/portfolio/');
-      ReactGA.pageview("/portfolio/#/portfolio/projects");
+import "./App.css";
+import Home from "./components/Home/Home";
+import Contact from "./components/Contact/Contact";
+import About from "./components/About/About";
+import Projects from "./components/Projects/Projects";
 
-class App extends Component {
- 
-  render() {
-    return (
-      <HashRouter basename="/portfolio">
-        <div className="container">
-          <Navbar />
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route path="/contact" component={Contact} />
-            <Route path="/projects" component={Projects} />
-            <Route path="/about" component={About} />
-          </Switch>
-        </div>
-      </HashRouter>
-    )
-  }
-}
+const App = () => {
+  return (
+    <HashRouter basename="/portfolio">
+      <div className="container">
+        <Navbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/projects" component={Projects} />
+          <Route path="/about" component={About} />
+        </Switch>
+      </div>
+    </HashRouter>
+  );
+};
 
 export default App;
